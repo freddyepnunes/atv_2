15a87d9c28dc024a75de38ffd20f0c2629e4267a
freddyepnunes <freddye_exatos@hotmail.com>
2022-03-14 21:47:12 -0300

    feat(dados)
    Adicionando informações dos integrantes do grupo.

---
+++ atv 1/src/atv/pkg1/Atv1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atv.pkg1;
+
+/**
+ *
+ * @author alunocmc
+ */
+public class Atv1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Freddye Pontes Nunes");
+        System.out.println("11211202211");
+        System.out.println("29 anos");
+        System.out.println("Gabriel Boschi tanikawa");
+        System.out.println("11211100784");
+        System.out.println("18 anos");
+        
+    }
+    
+}
